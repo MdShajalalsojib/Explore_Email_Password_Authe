401167c992042dd4b39fdc9caf50c2156da4253b
MdShajalalsojib <shahjalalsojib088@gmail.com>
2025-10-09 23:26:04 +0600

    add the home pase bes

--- Explore_Email_Password_Authe/src/Components/Login/Login.jsx
+++ Explore_Email_Password_Authe/src/Components/Login/Login.jsx
@@ -3,7 +3,12 @@ import React from 'react';
 const Login = () => {
     return (
         <div>
-            <h2>This is login</h2>
+            <div className='bg-gray-600 rounded-2xl  w-2xl text-center mt-10 ml-[400px] '>
+             <h1 className='text-2xl font-bold text-center'>Register pless </h1>
+             <h2>Shajalal</h2>
+             <h3>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Officiis quod atque vitae nihil recusandae dolorum voluptatum temporibus natus minus sunt?</h3>
+
+             </div>
         </div>
     );
 };
--- Explore_Email_Password_Authe/src/Components/Register/Register.jsx
+++ Explore_Email_Password_Authe/src/Components/Register/Register.jsx
@@ -3,7 +3,10 @@ import React from 'react';
 const Register = () => {
     return (
         <div>
-             <div className='h-1/2 rou'>
+             <div className='bg-gray-600 rounded-2xl  w-2xl text-center mt-10 ml-[400px] '>
+             <h1 className='text-2xl font-bold text-center'>Register pless </h1>
+             <h2>Shajalal</h2>
+             <h3>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Officiis quod atque vitae nihil recusandae dolorum voluptatum temporibus natus minus sunt?</h3>
 
              </div>
         </div>
