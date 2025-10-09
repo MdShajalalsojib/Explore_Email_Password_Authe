4fd8b38d872f44e2c2e6a6c9065516451d40e66d
MdShajalalsojib <shahjalalsojib088@gmail.com>
2025-10-10 00:26:22 +0600

    the facefook link the butten

--- Explore_Email_Password_Authe/src/Components/Login/Login.jsx
+++ Explore_Email_Password_Authe/src/Components/Login/Login.jsx
@@ -7,7 +7,7 @@ const Login = () => {
              <h1 className='text-2xl font-bold text-center'>Register pless </h1>
              <h2>Shajalal</h2>
              <h3>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Officiis quod atque vitae nihil recusandae dolorum voluptatum temporibus natus minus sunt?</h3>
-
+                  
              </div>
         </div>
     );
