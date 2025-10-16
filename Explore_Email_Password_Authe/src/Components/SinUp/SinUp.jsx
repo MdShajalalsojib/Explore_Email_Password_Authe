5ad0c66d68b4ace61105d7a21270bddee0762ecb
MdShajalalsojib <shahjalalsojib088@gmail.com>
2025-10-16 22:08:42 +0600

    full comput the sinup step

--- Explore_Email_Password_Authe/src/Components/SinUp/SinUp.jsx
+++ Explore_Email_Password_Authe/src/Components/SinUp/SinUp.jsx
@@ -1,23 +1,45 @@
 import { createUserWithEmailAndPassword } from "firebase/auth";
 import React, { useState } from "react";
 import { auth } from "../../firebase.init";
+import { FaEye, FaEyeSlash } from "react-icons/fa";
+import { Link } from "react-router";
 
 const SinUp = () => {
 
+  const [success, setSuccess] = useState(false);
+  const [showPassword, setShowPassword ] = useState(false);
+  
     const [errorMessage, setErrorMessage] = useState('')
 
     const handleSignUp = e =>{
         e.preventDefault();
         const email = e.target.email.value;
         const password = e.target.password.value;
-        console.log(email,password);
+        const terms = e.target.terms.checked;
+        console.log(email,password, terms);
+
+        setSuccess(false);
 
         setErrorMessage('');
+
+        if(!terms){
+          setErrorMessage('Please accept Out Terms and conditions');
+          return;
+        }
+
+        //password validate 
+        const passwordRegExp = /(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}/
+        if(passwordRegExp.test(password) === false){
+          setErrorMessage('Password must have one lowercase, one uppercase, one digit and 6 characters or LockManager.')
+
+            return;
+        }
 
         //create user
         createUserWithEmailAndPassword(auth, email, password)
         .then(result =>{
             console.log(result);
+            setSuccess(true);
         })
         .catch(error =>{
             console.log(error);
@@ -29,26 +51,47 @@ const SinUp = () => {
 
   return (
     <div>
-            <div className="card bg-base-100 mx-auto mt-10 w-full max-w-sm shrink-0 shadow-2xl">
+            <div className="card bg-gray-500 mx-auto mt-10 w-full max-w-sm shrink-0 shadow-2xl">
                  <h1 className="text-3xl font-bold p-5 text-center">SinUp</h1>
               <div className="card-body">
                 <form onSubmit={handleSignUp}>
                   <label className="label">Email</label>
                   <input type="email" name="email" className="input" placeholder="Email" />
-                  <label className="label">Password</label>
-                  <input
-                    type="password"
+                  <label className="label mt-3">Password</label>
+                  <div className="relative">
+                     <input
+                     type={showPassword ? 'text':'password'}
                     name="password"
                     className="input mt-4"
                     placeholder="Password"
+
                   />
-                  <div>
+                  <button 
+                  onClick={() => {setShowPassword(!showPassword) }}
+                  className="btn btn-xs absolute top-6 right-6">
+                    {
+                      showPassword ? <FaEye></FaEye> : <FaEyeSlash></FaEyeSlash>
+                    }
+                     </button>
+                  </div>
+                  <div className="mt-4">
                     <a className="link link-hover">Forgot password?</a>
                   </div>
+
+                  <label className="label mt-5">
+                   <input type="checkbox" name="terms"  className="checkbox" />
+                         Accept Terms and Conditions
+                    </label>
+                    <br/>
                   <button className="btn btn-neutral mt-4 w-[320px]">SinUp</button>
                 </form>
+                  <p>Already have an account? Please <Link className="text-blue-400" to="/login">login</Link></p>
+
                 {
                     errorMessage && <p className="text-red-400">{errorMessage}</p>
+                }
+                {
+                  success && <p className="text-green-500">User has created successfully</p>
                 }
               </div>
             </div>
